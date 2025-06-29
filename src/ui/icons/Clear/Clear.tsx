725c35ef0baacbde9d8b14231f85e9effa559b42
Yaroslav Mikhaylov <sponaproz@gmail.com>
2025-06-29 10:03:45 +0300

    fix: add roles to svgs

--- src/ui/icons/Clear/Clear.tsx
+++ src/ui/icons/Clear/Clear.tsx
@@ -4,7 +4,7 @@ import { IconBaseProps } from '@app-types/common';
 import { SvgBase } from '@ui/SvgBase';
 
 export const Clear: FC<IconBaseProps> = ({ size, ...rest }) => (
-    <SvgBase size={size} {...rest}>
+    <SvgBase size={size} role='img' {...rest}>
         <path
             d="M1.16675 20.3334L10.5001 11.0001M10.5001 11.0001L19.8334 1.66675M10.5001 11.0001L1.16675 1.66675M10.5001 11.0001L19.8334 20.3334"
             stroke="white"
--- src/ui/icons/Cross/Cross.tsx
+++ src/ui/icons/Cross/Cross.tsx
@@ -4,7 +4,7 @@ import { IconBaseProps } from '@app-types/common';
 import { SvgBase } from '@ui/SvgBase';
 
 export const Cross: FC<IconBaseProps> = ({ size, ...rest }) => (
-    <SvgBase size={size} {...rest}>
+    <SvgBase size={size} role="img" {...rest}>
         <path
             d="M6.66675 25.3332L16.0001 15.9998M16.0001 15.9998L25.3334 6.6665M16.0001 15.9998L6.66675 6.6665M16.0001 15.9998L25.3334 25.3332"
             stroke="currentColor"
